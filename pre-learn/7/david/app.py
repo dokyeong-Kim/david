96428e862c61fc162e831a532134e2e2cea86072
dokyeong-Kim <tpxm0505@gmail.com>
2025-08-30 13:21:52 +0900

    test1 함수 코드를 삭제

--- pre-learn/7/david/app.py
+++ pre-learn/7/david/app.py
@@ -6,10 +6,6 @@ import datetime
 import re
 import socket  # socket 모듈을 import합니다.
 
-@app.route("/test4")
-def test2():
-    return render_template('test4.html')
-
 # 사용자 정의 예외 클래스 정의 (입력 검증 실패 시 사용)
 class ValidationError(Exception):
     pass
